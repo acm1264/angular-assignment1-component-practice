2cf5a78dc9d53b374fb5d7195da867af6c76aa53
Andrew Maurice <andromulin@gmail.com>
2022-11-29 11:49:18 -0600

    assignment complete

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: `<h2>Success Message!!!!</h2>`,
+  styles: [`
+    h2{
+      color: green;
+    }
+  `]
+})
+export class SuccessAlertComponent {
+
+}
